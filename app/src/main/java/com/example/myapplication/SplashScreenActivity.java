32c854eb8f6e4757a02875b0885c600955004b8c
Arial0980 <arial95123@students.kinneret.ac.il>
2023-02-10 21:33:53 +0200

    projuct0.5

--- app/src/main/java/com/example/myapplication/SplashScreenActivity.java
+++ app/src/main/java/com/example/myapplication/SplashScreenActivity.java
@@ -1,4 +1,5 @@
 package com.example.myapplication;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityOptions;
@@ -6,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 public class SplashScreenActivity extends AppCompatActivity {
@@ -15,17 +18,10 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_splash_screen);
-
         ImageView splashImage = findViewById(R.id.imageView);
-        ScaleAnimation scaleAnimation = new ScaleAnimation(
-                1.0f, 0.5f,
-                1.0f, 0.5f,
-                Animation.RELATIVE_TO_SELF, -0.35f,
-                Animation.RELATIVE_TO_SELF, 0.3f
-        );
-        scaleAnimation.setDuration(4000);
-        scaleAnimation.setFillAfter(true);
-        splashImage.startAnimation(scaleAnimation);
+        createDelay(5,splashImage);
+
+        animation2(splashImage);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -36,6 +32,40 @@ public class SplashScreenActivity extends AppCompatActivity {
                 startActivity(intent, options.toBundle());
                 finish();
             }
-        }, 4000);
+        }, 8000);
     }
+    public void animation1( ImageView splashImage)
+    {
+        ScaleAnimation scaleAnimation = new ScaleAnimation(
+                1.0f, 0.5f,
+                1.0f, 0.5f,
+                Animation.RELATIVE_TO_SELF, -0.35f,
+                Animation.RELATIVE_TO_SELF, 0.3f
+        );
+        scaleAnimation.setDuration(3000);
+        scaleAnimation.setFillAfter(true);
+        splashImage.startAnimation(scaleAnimation);
+    }
+    public void animation2( ImageView splashImage)
+    {
+        RotateAnimation rotate = new RotateAnimation(
+                0, 360,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+
+        rotate.setRepeatCount(Animation.INFINITE);
+        rotate.setInterpolator(new LinearInterpolator());
+        rotate.setDuration(2500);
+        splashImage.startAnimation(rotate);
+    }
+    private void createDelay(int seconds,ImageView splashImage) {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                animation1(splashImage);
+            }
+        }, seconds * 1000);
+    }
+
 }
