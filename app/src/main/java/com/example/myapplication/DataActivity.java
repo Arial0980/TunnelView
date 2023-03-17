84e24fb4540fbdd027dcbc52b531e9d2c0f1fe84
Arial0980 <arial95123@students.kinneret.ac.il>
2023-03-17 21:07:08 +0200

    projuct 1.3 test Map

--- app/src/main/java/com/example/myapplication/DataActivity.java
+++ app/src/main/java/com/example/myapplication/DataActivity.java
@@ -4,7 +4,10 @@ import static android.content.ContentValues.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 
@@ -20,14 +23,20 @@ import com.google.firebase.database.ValueEventListener;
 
 
 public class DataActivity extends AppCompatActivity {
-    private DatabaseReference myRefDis1,myRefDis2,myRefDis3;
-    private int dis1,dis2,dis3;
-
+    private DatabaseReference myRefDis1,myRefDis2,myRefDis3,myRefDidW;
+    private int dis1,dis2,dis3,didW;
+    private Bitmap image;
+    private final int FIVE_SECONDS = 5000;
+    private int WIDTH=100,HEIGHT=100,x=50,y=100;
+    private Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
         getSupportActionBar().hide();
+
+
+
 
         TextView right=findViewById(R.id.Right_textView);
         TextView left=findViewById(R.id.Left_textView);
@@ -87,8 +96,70 @@ public class DataActivity extends AppCompatActivity {
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
-    }
 
+        myRefDidW = database.getReference("test/didW");
+        myRefDidW.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                didW = dataSnapshot.getValue(Integer.class);
+                Log.d(TAG, "Value is: " + didW);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        image = Bitmap.createBitmap(WIDTH, HEIGHT, Bitmap.Config.ARGB_8888);
+        scheduleSendLocation();
+    }
+    public void scheduleSendLocation() {
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                drowMap(didW);
+                handler.postDelayed(this, FIVE_SECONDS);
+            }
+        }, FIVE_SECONDS);
+    }
+    public void drowMap(int didWhat){
+        int z;
+        if(didWhat==0){
+            image.setPixel(x-5,y, Color.BLACK);
+            image.setPixel(x+5,y, Color.BLACK);
+            image.setPixel(x,y, Color.YELLOW);
+            y++;
+
+        }
+        image.setPixel(x,y, Color.YELLOW);
+        if(didWhat==1){
+            image.setPixel(x,y, Color.YELLOW);
+            for(int i=0; i<=3; i++)
+                image.setPixel(x+5,y-i, Color.WHITE);
+            for(int i=0; i<=3; i++)
+                image.setPixel(x-5,y+i, Color.BLACK);
+            for(int i=-5; i<=5; i++)
+                image.setPixel(x-i,y+3, Color.BLACK);
+            z=x;
+            x=y;
+            y=z;
+        }
+        if(didWhat==2){
+            image.setPixel(x,y, Color.YELLOW);
+            for(int i=0; i<=3; i++)
+                image.setPixel(x-5,y-i, Color.WHITE);
+            for(int i=0; i<=3; i++)
+                image.setPixel(x+5,y+i, Color.BLACK);
+            for(int i=-5; i<=5; i++)
+                image.setPixel(x-i,y+3, Color.BLACK);
+            z=x;
+            x=y;
+            y=z;
+        }
+    }
     public void exit(View view) {
         Intent intent = new Intent(DataActivity.this, MainActivity.class);
         startActivity(intent);
